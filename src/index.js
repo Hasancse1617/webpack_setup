989f0a7ce4e3c3aaae4243e338f6973a675e860f
Hasan Ali <hasancseiu16@gmail.com>
2023-05-09 15:00:09 +0600

    updated files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import _ from "lodash";
 import "./style.css";
+import image from "./hasan.jpg";
 
 function component() {
     const element = document.createElement('div');
   
     // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['Hello', 'Hasan Ali'], ' ');
-  
+    const myImage = new Image();
+    myImage.src = image;
+    element.appendChild(myImage);
     return element;
   }
   
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
@@ -12,11 +14,21 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
   devServer: {
     static: path.join(__dirname, "dist"),
     compress: true,
     port: 3000
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+      filename: 'index.html'
+    }),
+  ],
 };
